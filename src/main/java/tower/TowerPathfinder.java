7e8babdb9a804d2386b86e4a4abc80c6322df1c6
Darkness <inflexibled@gmail.com>
2023-06-20 13:22:14 +0300

    фикс

--- src/main/java/tower/TowerPathfinder.java
+++ src/main/java/tower/TowerPathfinder.java
@@ -26,6 +26,7 @@ public class TowerPathfinder extends Pathfinder {
         costTypes.set(costLegs, (team, tile) -> (PathTile.allDeep(tile) || PathTile.legSolid(tile)) ? impassable : 1 +
                 (PathTile.deep(tile) ? notPath : 0) +
                 (PathTile.damages(tile) ? 50 : 0) +
+                (PathTile.nearLegSolid(tile) ? 50 : 0) +
                 (PathTile.nearSolid(tile) ? 10 : 0)
         );
 
@@ -38,14 +39,15 @@ public class TowerPathfinder extends Pathfinder {
 
     @Override
     public int packTile(Tile tile) {
-        boolean nearLiquid = false, nearSolid = false, nearGround = false, allDeep = tile.floor().isDeep();
+        boolean nearLiquid = false, nearSolid = false, nearLegSolid = false, nearGround = false, allDeep = tile.floor().isDeep();
 
         for (int i = 0; i < 4; i++) {
             var other = tile.nearby(i);
             if (other == null) continue;
 
             if (other.floor().isLiquid) nearLiquid = true;
-            if (other.solid() || !isPath(other)) nearSolid = true; // !isPath -> nearSolid
+            if (other.solid() || !isPath(other)) nearSolid = true;
+            if (other.legSolid() || !isPath(other)) nearLegSolid = true;
             if (!other.floor().isLiquid) nearGround = true;
             if (!other.floor().isDeep()) allDeep = false;
         }
@@ -55,12 +57,13 @@ public class TowerPathfinder extends Pathfinder {
                 tile.getTeamID(),
                 tile.solid(),
                 tile.floor().isLiquid,
-                tile.staticDarkness() > 1 || tile.floor().solid, // staticDarkness > 1 or floor.solid -> legSolid
+                tile.legSolid(),
                 nearLiquid,
                 nearGround,
                 nearSolid,
-                tile.floor().isDeep() || !isPath(tile), // !isPath -> deep
-                tile.floor().damageTaken > 0f || !isPath(tile), // !isPath -> damages
+                nearLegSolid,
+                tile.floor().isDeep() || !isPath(tile),
+                tile.floor().damageTaken > 0f || !isPath(tile),
                 allDeep,
                 tile.block().teamPassable
         );
